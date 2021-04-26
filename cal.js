794c646f6c29673737ed1098f284c4f624616201
landon-is-on <landonsmothers24@gmail.com>
2021-04-26 18:42:20 -0400

    was able to make  it add numbers

--- cal.js
+++ cal.js
@@ -2,19 +2,23 @@ class Calculator {
     constructor(previousOperandTextElement, currentOperandTextElement) {
         this.previousOperandTextElement = previousOperandTextElement
         this.currentOperandTextElement = currentOperandTextElement
+        this.clear()
     }
 
 
 clear() {
-
+this.currentOperand = ''
+this.previousOperand = ''
+this.operation = undefined
 }
 
 delete() {
 
 }
 
-appendNumber() {
-
+appendNumber(number) {
+if (number === '.' && this.currentOperand.includes('.')) return
+    this.currentOperand = this.currentOperand.toString() + number.toString()
 }
 
 
@@ -26,15 +30,32 @@ compute() {
     
 }
 
-updataDisplay() {
-    
+updateDisplay() {
+    this.currentOperandTextElement.innerText = this.currentOperand
 }
 
 }
 const numberButtons = document.querySelectorAll('[data-number]')
-const operstionButtons = document.querySelectorAll('[data-operation]')
-const equalsButton = document.querySelectorAll('[data-equals]')
-const deleteButton = document.querySelectorAll('[data-equals]')
-const allClearButton = document.querySelectorAll('[data-equals]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]')
 const previousOperandTextElement = document.querySelector('[data-previous-operand]')
 const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
+const calculator = new Calculator(previousOperandTextElement,
+    currentOperandTextElement)
+
+    numberButtons.forEach(button => {
+        button.addEventListener('click', () =>{
+            calculator.appendNumber(button.innerText)
+            calculator.updateDisplay()
+        })
+    })
+
+    numberButtons.forEach(button => {
+        button.addEventListener('click', () =>{
+            calculator.appendNumber(button.innerText)
+            calculator.updateDisplay()
+        })
+    })
